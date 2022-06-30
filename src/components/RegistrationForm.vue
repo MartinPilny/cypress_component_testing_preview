18d0b50f502ef85415ec2308dca96febe2b05b11
Martin Pilný <martin.pilny@quanti.cz>
2022-06-30 11:05:31 +0200

    UPDATE RegistrationForm.vue - data-test-ids

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -3,41 +3,72 @@
   <form class="greetings">
     <label for="email"><b>Email</b></label
     ><br />
-    <input name="email" type="email" v-model="email" @blur="v$.email.$touch" />
+    <input
+      data-test-id="email-input"
+      name="email"
+      type="email"
+      v-model="email"
+      @blur="v$.email.$touch"
+    />
     <br />
-    <div v-if="v$.email.$error">Email field has an error.</div>
+    <div data-test-id="email-invalid" v-if="v$.email.$error">
+      Email field has an error.
+    </div>
 
     <label for="password"><b>Password</b></label
     ><br />
     <input
+      data-test-id="password-input"
       name="password"
       type="password"
       v-model="password"
       @blur="v$.password.$touch"
     />
     <br />
-    <div v-if="v$.password.minLength.$invalid">Password is too short.</div>
-    <div v-if="v$.password.containsUppercase.$invalid">
+    <div
+      data-test-id="password-invalid-length"
+      v-if="v$.password.minLength.$invalid"
+    >
+      Password is too short.
+    </div>
+    <div
+      data-test-id="password-invalid-uppercase"
+      v-if="v$.password.containsUppercase.$invalid"
+    >
       Password does not contain upper case letter.
     </div>
-    <div v-if="v$.password.containsLowercase.$invalid">
+    <div
+      data-test-id="password-invalid-lowercase"
+      v-if="v$.password.containsLowercase.$invalid"
+    >
       Password does not contain lower case letter.
     </div>
-    <div v-if="v$.password.containsNumber.$invalid">
+    <div
+      data-test-id="password-invalid-number"
+      v-if="v$.password.containsNumber.$invalid"
+    >
       Password does not contain number.
     </div>
 
     <label for="passwordConfirm"><b>Password Confirm</b></label
     ><br />
     <input
+      data-test-id="password-confirm-input"
       name="passwordConfirm"
       type="password"
       v-model="passwordConfirm"
       @blur="v$.passwordConfirm.$touch"
     />
     <br />
-    <div v-if="v$.passwordConfirm.$error">Password are not same.</div>
-    <button :disabled="v$.$invalid" type="submit">Register</button>
+    <div
+      data-test-id="password-confirm-invalid"
+      v-if="v$.passwordConfirm.$error"
+    >
+      Password are not same.
+    </div>
+    <button data-test-id="submit-button" :disabled="v$.$invalid" type="submit">
+      Register
+    </button>
   </form>
 </template>
 
